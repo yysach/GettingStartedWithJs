7e55d7eec4ebdab1644da4704fd9f76b588b91ee
yysach <yysachin11@gmail.com>
2020-07-12 19:14:22 +0530

    serving html page

--- createServer.js
+++ createServer.js
@@ -2,9 +2,13 @@ var http = require('http')
 var fs = require('fs')
 var server = http.createServer(function(req,res){
     console.log("request made from"+req.url)
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    var readStream = fs.createReadStream(__dirname+'/file.txt','utf8')
-    readStream.pipe(res);
+    console.log('request method '+req.method)
+    res.writeHead(200,{'Content-Type':'text/html'});
+    var readHTMLStream = fs.createReadStream(__dirname+'/template/index.html','utf8')
+    readHTMLStream.pipe(res);
+    
+  
+
     //res.end('<h1>hello create server on 3000<h1>');
 });
 
