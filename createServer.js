9e06f46f251c8b0ee681db5452203c026f0bb1f6
yysach <yysachin11@gmail.com>
2020-07-12 19:49:40 +0530

    serving html+css

--- createServer.js
+++ createServer.js
@@ -1,15 +1,31 @@
 var http = require('http')
 var fs = require('fs')
+
+fs.readFile('./template/index.html', function(err, data) {
+    if (err){
+        throw err;
+    }
+    htmlFile = data;
+});
+
+fs.readFile('./template/style.css', function(err, data) {
+    if (err){
+        throw err;
+    }
+    cssFile = data;
+});
 var server = http.createServer(function(req,res){
     console.log("request made from"+req.url)
     console.log('request method '+req.method)
-    res.writeHead(200,{'Content-Type':'text/html'});
-    var readHTMLStream = fs.createReadStream(__dirname+'/template/index.html','utf8')
-    readHTMLStream.pipe(res);
-    
-  
-
-    //res.end('<h1>hello create server on 3000<h1>');
+    switch (req.url) {
+        case "/style.css" :
+            res.writeHead(200, {"Content-Type": "text/css"});
+            fs.createReadStream(__dirname+'/template/style.css','utf8').pipe(res)
+            break;
+        default :    
+            res.writeHead(200, {"Content-Type": "text/html"});
+            fs.createReadStream(__dirname+'/template/index.html','utf8').pipe(res)
+    }
 });
 
 server.listen(3001,'localhost');
